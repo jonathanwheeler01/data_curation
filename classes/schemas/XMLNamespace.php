9b074809c6c0086586c8e4e22a23f20463ecc609
Robert Olendorf <drolendorf@gmail.com>
2011-11-12 22:43:56 -0700

    created a generic XMLElement Class and added some helper functions to aXMLElement aid in adding attributes and namespaces to elements

---
+++ classes/schemas/XMLNamespace.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * XMLNamespace Bean type structure for holding XML namespace data;
+ *
+ * @author Rob
+ */
+class XMLNamespace {
+  public $uri;
+  public $location;
+  public $prefix;
+}
+
+?>
