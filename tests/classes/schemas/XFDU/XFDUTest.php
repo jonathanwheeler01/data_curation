1b6916b591113b1048653626d6b5ba9a1dc62454
Robert Olendorf <drolendorf@gmail.com>
2011-11-21 00:13:18 -0700

    added a handful of comments

--- tests/classes/schemas/XFDU/XFDUTest.php
+++ tests/classes/schemas/XFDU/XFDUTest.php
@@ -145,40 +145,51 @@ class XFDUTest extends PHPUnit_Framework_TestCase {
   }
   
   /**
-   * 
+   * A really long test, that probably violates a lot of the rules of unit
+   * testing, but ends up being a pretty good test of the overall 
+   * document structure.
    */
   public function testDefault_get_as_DOM() {
+    // Set the various parameters that will be needed.
     $id = 'test';
     $sequenceSize = 2;
     $sequencePosition = 1;
     $value = 'test';
-    $dom = new DOMDocument('1.0', 'UTF-8');
     
-    $packageHeader = new PackageHeader();
-    $packageHeader->set_id($value);
+    $dom = new DOMDocument('1.0', 'UTF-8');                                     // create the dom document
     
-    $volumeInfo = new VolumeInfo();
-    
+    // Child of volumeInfo
     $specificationVersion = $dom->createElement('specificationVersion');
     $specificationVersion->appendChild($dom->createTextNode('1.0'));
     
+    // Child of volumeInfo
     $sequenceInformation = new SequenceInformation();
     $sequenceInformation->set_sequencePosition($sequencePosition);
     $sequenceInformation->set_sequenceSize($sequenceSize);
     $sequenceInformation->set_value($value);
     
+    // Child of packageHeader
+    $volumeInfo = new VolumeInfo();
     $volumeInfo->set_sequenceInformation($sequenceInformation);
     
+    //Child of environmentInfo
     $xmlData = new XMLData();
+    
+    //Child of environmentInfo
     $extension = new Extension();
     
+    //Child of packageHeader
     $environmentInfo = new EnvironmentInfo();
     $environmentInfo->set_xmlData($xmlData);
     $environmentInfo->set_extension($extension);
     
+    //Child of XFDU
+    $packageHeader = new PackageHeader();                                       
+    $packageHeader->set_id($value);
     $packageHeader->set_volumeInfo($volumeInfo);
     $packageHeader->set_environmentInfo($environmentInfo);
     
+    // Root element (XFDU)
     $this->object->set_packageHeader($packageHeader);
     $this->object->set_informationPackageMap(new InformationPackageMap);
     $this->object->set_metadataSection(new MetadataSection());
