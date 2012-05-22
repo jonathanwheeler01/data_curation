eb3402cf73f0245709e9ce6736be3c6586e0d883
Robert Olendorf <drolendorf@gmail.com>
2012-05-22 11:25:15 -0600

    pulled front end out of this project as its a pain to keep it updated with a drupal instance. I created a separate project for the drupal front end.

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once 'curation_tool.inc';
 $dom = new DOMDocument('1.0', 'UTF-8');
 $settings = new XFDUSetup();
 $settings->root = 'c:/repository/olendorf/fake';
-$settings->xmlData = $dom->createElement('stuff');
 
 $dirProc = new DirectoryProcessor($settings);
 $dirProc->process_dataset();
