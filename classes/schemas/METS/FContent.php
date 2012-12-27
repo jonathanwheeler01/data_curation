c9d474ed968a1e878e8394baecf7d03e33fbaf2e
Jon Wheeler <jonathan.wheeler01@gmail.com>
2012-12-27 14:57:59 -0700

    added mets classes fcontent and xmldata

---
+++ classes/schemas/METS/FContent.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ *
+ * @author Jon Wheeler
+ */
+class FContent extends aMETSElement{
+	/*
+	 * @var string
+	 */
+	protected $ID;
+
+	/*
+	 * @var string
+	 * */
+	protected $USE;
+	
+	/*
+	 * @var XMLData
+	 * */
+	protected $XMLData;
+	
+	/*
+	 * 
+	 * @var binData;
+	 * 
+	 * */
+	protected $binData;
+	
+	public function __construct() {
+		$this->fcontent = array();
+	}
+	
+	/*
+	 * @param string $ID
+	 */
+	public function set_ID($ID){
+		if($this->validate_id($ID)){
+			$this->ID = $ID;
+		}
+		else {
+			throw new InvalidIDTokenException($ID);
+		}
+	}
+	
+	/*
+	 * @return string
+	 */
+	public function get_ID() {
+		return $this->ID;
+	}
+	
+	/*
+	 * @return boolean
+	 */
+	public function isset_ID() {
+		return (isset($this->ID) && !empty($this->ID));
+	}
+	
+	/*
+     * @param string $USE
+     */
+    public function set_USE($USE){
+        $this->USE = $USE;
+    }
+    
+    /*
+     * @return string
+     */
+    public function get_USE() {
+        return $this->USE;
+    }
+    
+    /*
+     * @return boolean
+     */
+    public function isset_USE() {
+        return (isset($this->USE) && !empty($this->USE));
+    }
+    
+    /*
+     * @param string $XMLData
+    */
+    public function set_XMLData($XMLData){
+    	$this->XMLData = $XMLData;
+    }
+    
+    /*
+     * @return string
+    */
+    public function get_XMLData() {
+    	return $this->XMLData;
+    }
+    
+    /*
+     * @return boolean
+    */
+    public function isset_XMLData() {
+    	return (isset($this->XMLData) && !empty($this->XMLData));
+    }
+    
+    /*
+     * @param string $binData
+    */
+    public function set_binData($binData){
+    	$this->binData = $binData;
+    }
+    
+    /*
+     * @return string
+    */
+    public function get_binData() {
+    	return $this->binData;
+    }
+    
+    /*
+     * @return boolean
+    */
+    public function isset_binData() {
+    	return (isset($this->binData) && !empty($this->binData));
+    }
+    
+    public function get_as_DOM($prefix = NULL) {
+    	$dom = new DOMDocument($this->XMLVersion, $this->XMLEncoding);
+    
+    	if($prefix !== NULL) {
+    		$fcontent = $dom->createElement($prefix.':'.$this->first_to_lower(get_class($this)));
+    		$fcontent = $dom->createElementNS('http://www.loc.gov/METS/', $prefix.':fcontent');
+    	}
+    	else {
+    		$fcontent = $dom->createElementNS('http://www.loc.gov/METS/', 'mets:'.$this->first_to_lower(get_class($this)));
+    	}
+    
+    	// handle attributes
+    	if($this->isset_ID()) {
+    		$fcontent->setAttribute('ID', $this->ID);
+    	}
+    
+    	if($this->isset_USE()) {
+    		$fcontent->setAttribute('USE', $this->USE);
+    	}
+    	
+    	// handle child elements
+    	if($this->isset_binData()){
+    		$fcontent->appendChild($dom->importNode($this->binData->get_as_DOM(), TRUE));
+    	}
+    	
+    	if($this->isset_XMLData()){
+    		$fcontent->appendChild($dom->importNode($this->XMLData->get_as_DOM(), TRUE));
+    	}
+    
+    		return $fcontent;
+    
+    	}
+    
+}    
+
+?>
---
+++ classes/schemas/METS/XMLData.php
@@ -0,0 +1,70 @@
+<?php
+require_once dirname(__FILE__) . '/../../../curation_tool.inc';
+
+/**
+ * A wrapper to contain arbitrary XML content.
+ *
+ * @author Rob Olendorf
+ */
+class XMLData extends aMETSElement{
+  /**
+   * Freeform or structured XML. May be stored an any format, String DOM etc.
+   * @var mixed
+   */
+  protected $any;
+  
+  /**
+   *
+   * @param string $any XML as DOMElement or DOMNodeList
+   */
+  public function set_any($any) {
+    if(get_class($any) != 'DOMElement' && get_class($any) != 'DOMNodeList') {
+      $message = 'The METS Exception element must be an instance of a DOMNode, '.
+                 'instance of '.  get_class($any).' given.';
+      $code = 0;
+      $previous = NULL;
+      throw new InvalidArgumentException($message, $code, $previous);
+    }
+    $this->any = $any;
+  }
+  
+  /**
+   *
+   * @return Mixed
+   */
+  public function get_any() {
+    return $this->any;
+  }
+  
+  /**
+   *
+   * @return boolean
+   */
+  public function isset_any() {
+    return (isset($this->any) && !empty($this->any));
+  }  
+  
+  /**
+   *
+   * @param type $prefix 
+   * @return DOMElement;
+   */
+  public function get_as_DOM($prefix = NULL) {
+    $dom = new DOMDocument($this->XMLVersion, $this->XMLEncoding);
+
+    $xmlData = $dom->createElement('xmlData');
+
+    if(get_class($this->any) == 'DOMNode' || get_class($this->any) == 'DOMElement' ) {
+      $xmlData->appendChild($dom->importNode($this->any, TRUE));
+    }
+    else if(get_class($this->any) == 'DOMNodeList'){
+      for($i = 0; $i < $this->any->length; $i++) {
+        $xmlData->appendChild($dom->importNode($this->any->item($i), TRUE));
+      }
+    }
+    
+    return $xmlData;
+  }
+}
+
+?>
