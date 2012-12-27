6a86062d0fabf443ba77d96df981188d9723783e
Jon Wheeler <jonathan.wheeler01@gmail.com>
2012-12-27 15:09:53 -0700

    added bindata class

---
+++ classes/schemas/METS/BinData.php
@@ -0,0 +1,27 @@
+<?php
+require_once dirname(__FILE__) . '/../../../curation_tool.inc';
+
+/**
+ * A place holder class for bin64 data.
+ * Currently an XML wrapper. 
+ *
+ * @author Jon Wheeler
+ */
+class BinData extends aMETSElement{
+  
+	/**
+	 *
+	 * @param type $prefix
+	 * @return DOMElement;
+	 */
+	public function get_as_DOM($prefix = NULL) {
+		$dom = new DOMDocument($this->XMLVersion, $this->XMLEncoding);
+	
+		$binData = $dom->createElement('xmlData');
+	
+		return $xmlData;
+	}
+	
+}
+
+?>
